62675b47a8ce938873b5bdfb9fee3f675e92c420
Anon Sols <soloschmail@gmail.com>
2024-02-09 02:51:25 +0100

    implement context api for state management

--- src/App.tsx
+++ src/App.tsx
@@ -1,5 +1,6 @@
-import { createContext, useEffect, useState } from "react";
+import { useContext, useEffect, useState } from "react";
 import { faker } from "@faker-js/faker";
+import PostProvider, { PostContext } from "./PostContext";
 
 function createRandomPost() {
   return {
@@ -8,32 +9,10 @@ function createRandomPost() {
   };
 }
 
-const PostContext = createContext();
-
 function App() {
-  const [posts, setPosts] = useState(() =>
-    Array.from({ length: 30 }, () => createRandomPost())
-  );
-  const [searchQuery, setSearchQuery] = useState("");
   const [isFakeDark, setIsFakeDark] = useState(false);
 
   // Derived state. These are the posts that will actually be displayed
-  const searchedPosts =
-    searchQuery.length > 0
-      ? posts.filter((post) =>
-          `${post.title} ${post.body}`
-            .toLowerCase()
-            .includes(searchQuery.toLowerCase())
-        )
-      : posts;
-
-  function handleAddPost(post: Post) {
-    setPosts((posts) => [post, ...posts]);
-  }
-
-  function handleClearPosts() {
-    setPosts([]);
-  }
 
   // Whenever `isFakeDark` changes, we toggle the `fake-dark-mode` class on the HTML element (see in "Elements" dev tool).
 
@@ -45,77 +24,48 @@ function App() {
   );
 
   return (
-    <PostContext.Provider
-      value={{
-        posts: searchedPosts,
-        onClearPosts: handleClearPosts,
-        searchQuery,
-        setSearchQuery,
-        onAddPosts: handleAddPost,
-      }}
-    >
-      <section>
-        <button
-          onClick={() => setIsFakeDark((isFakeDark) => !isFakeDark)}
-          className="btn-fake-dark-mode"
-        >
-          {isFakeDark ? "☀️" : "🌙"}
-        </button>
-
-        <Header
-          posts={searchedPosts}
-          onClearPosts={handleClearPosts}
-          searchQuery={searchQuery}
-          setSearchQuery={setSearchQuery}
-        />
-        <Main posts={searchedPosts} onAddPost={handleAddPost} />
-        <Archive onAddPost={handleAddPost} />
+    <section>
+      <button
+        onClick={() => setIsFakeDark((isFakeDark) => !isFakeDark)}
+        className="btn-fake-dark-mode"
+      >
+        {isFakeDark ? "☀️" : "🌙"}
+      </button>
+      <PostProvider>
+        <Header />
+        <Main />
+        <Archive />
         <Footer />
-      </section>
-    </PostContext.Provider>
+      </PostProvider>
+    </section>
   );
 }
 
-type postsProp = {
-  title: string;
-  body: string;
-};
+// type postsProp = {
+//   title: string;
+//   body: string;
+// };
 
-type headerProp = {
-  posts: postsProp[];
-  onClearPosts(): void;
-  searchQuery: string;
-  setSearchQuery: React.Dispatch<React.SetStateAction<string>>;
-};
+function Header() {
+  const { onClearPosts } = useContext(PostContext);
 
-function Header({
-  posts,
-  onClearPosts,
-  searchQuery,
-  setSearchQuery,
-}: headerProp) {
   return (
     <header>
       <h1>
         <span>⚛️</span>The Atomic Blog
       </h1>
       <div>
-        <Results posts={posts} />
-        <SearchPosts
-          searchQuery={searchQuery}
-          setSearchQuery={setSearchQuery}
-        />
-        <button onClick={onClearPosts}>Clear posts</button>
+        <Results />
+        <SearchPosts />
+        <button onClick={() => onClearPosts()}>Clear posts</button>
       </div>
     </header>
   );
 }
 
-type searchedPostsProp = {
-  searchQuery: string;
-  setSearchQuery: React.Dispatch<React.SetStateAction<string>>;
-};
-function SearchPosts({ searchQuery, setSearchQuery }: searchedPostsProp) {
+function SearchPosts() {
+  const { searchQuery, setSearchQuery } = useContext(PostContext);
+
   return (
     <input
       value={searchQuery}
@@ -130,24 +80,23 @@ type Post = {
   body: string;
 };
 
-function Results({ posts }: { posts: postsProp[] }) {
+function Results() {
+  const { posts } = useContext(PostContext);
   return <p>🚀 {posts.length} atomic posts found</p>;
 }
 
-type mainProp = {
-  posts: postsProp[];
-  onAddPost(post: Post): void;
-};
-function Main({ posts, onAddPost }: mainProp) {
+function Main() {
   return (
     <main>
-      <FormAddPost onAddPost={onAddPost} />
-      <Posts posts={posts} />
+      <FormAddPost />
+      <Posts />
     </main>
   );
 }
 
-function Posts({ posts }: { posts: postsProp[] }) {
+function Posts() {
+  const { posts } = useContext(PostContext);
+
   return (
     <section>
       <List posts={posts} />
@@ -155,9 +104,11 @@ function Posts({ posts }: { posts: postsProp[] }) {
   );
 }
 
-function FormAddPost({ onAddPost }: { onAddPost(post: Post): void }) {
+function FormAddPost() {
   const [title, setTitle] = useState("");
   const [body, setBody] = useState("");
+
+  const { onAddPost } = useContext(PostContext);
 
   const post = { title, body };
   const handleSubmit = function (e: React.FormEvent) {
@@ -185,7 +136,7 @@ function FormAddPost({ onAddPost }: { onAddPost(post: Post): void }) {
   );
 }
 
-function List({ posts }: { posts: postsProp[] }) {
+function List({ posts }: { posts: Post[] }) {
   return (
     <ul>
       {posts.map((post, i) => (
@@ -198,7 +149,8 @@ function List({ posts }: { posts: postsProp[] }) {
   );
 }
 
-function Archive({ onAddPost }: { onAddPost(post: Post): void }) {
+function Archive() {
+  const { onAddPost } = useContext(PostContext);
   // Here we don't need the setter function. We're only using state to store these posts because the callback function passed into useState (which generates the posts) is only called once, on the initial render. So we use this trick as an optimization technique, because if we just used a regular variable, these posts would be re-created on every render. We could also move the posts outside the components, but I wanted to show you this trick 😉
   const [posts] = useState(() =>
     // 💥 WARNING: This might make your computer slow! Try a smaller `length` first
---
+++ src/PostContext.tsx
@@ -0,0 +1,69 @@
+import { faker } from "@faker-js/faker";
+import { createContext, useState } from "react";
+
+type prop = {
+  children: React.ReactNode;
+};
+
+type Post = {
+  title: string;
+  body: string;
+};
+
+type PostContextProp = {
+  posts: Post[];
+  onClearPosts(): void;
+  searchQuery: string;
+  setSearchQuery: React.Dispatch<React.SetStateAction<string>>;
+  onAddPost: (post: Post) => void;
+};
+
+export const PostContext = createContext<PostContextProp>();
+
+function PostProvider({ children }: prop) {
+  const [posts, setPosts] = useState(() =>
+    Array.from({ length: 30 }, () => createRandomPost())
+  );
+  const [searchQuery, setSearchQuery] = useState("");
+
+  function createRandomPost() {
+    return {
+      title: `${faker.hacker.adjective()} ${faker.hacker.noun()}`,
+      body: faker.hacker.phrase(),
+    };
+  }
+
+  // Derived state. These are the posts that will actually be displayed
+  const searchedPosts =
+    searchQuery.length > 0
+      ? posts.filter((post) =>
+          `${post.title} ${post.body}`
+            .toLowerCase()
+            .includes(searchQuery.toLowerCase())
+        )
+      : posts;
+
+  function handleAddPost(post: Post) {
+    setPosts((posts) => [post, ...posts]);
+  }
+
+  function handleClearPosts() {
+    setPosts([]);
+  }
+
+  return (
+    <PostContext.Provider
+      value={{
+        posts: searchedPosts,
+        onClearPosts: handleClearPosts,
+        searchQuery,
+        setSearchQuery,
+        onAddPost: handleAddPost,
+      }}
+    >
+      <>{children}</>
+    </PostContext.Provider>
+  );
+}
+
+export default PostProvider;
